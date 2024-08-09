c0466dfe79380f482df69712137f03bcb7e414e1
Gabriel moreno <Ganterlix-Dev@users.noreply.github.com>
2024-08-09 02:06:51 +0200

    inicios de proyecto

--- windows.py
+++ windows.py
@@ -1,25 +1,39 @@
-import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import random
+import time
+import datetime
+from tkinter import messagebox
+import mysql.connector
+import tkinter as tk 
 
 app = tk.Tk()
 tk.Wm.wm_title(app, 'Administracion')
-
-def saludar():
-    print('hola')
-
-tk.Button(
-    app,
-    bg='red',
-    text='click',
-    font=('courier',14),
-    fg='blue',
-    #command=lambda: print('saludar'),
-    command=saludar
-).pack(
-    fill=tk.BOTH,
-    expand=True,
-)
+frm = ttk.Frame(app, padding=10)
 
 
 app.geometry('1000x800') #dimenciones
 app.configure(background='black') #fondo
 app.mainloop()
+
+# def saludar():
+#     print('hola')
+
+# tk.Button(
+#     app,
+#     bg='red',
+#     text='click',
+#     font=('courier',14),
+#     fg='blue',
+#     #command=lambda: print('saludar'),
+#     command=saludar
+# ).pack(
+#     fill=tk.BOTH,
+#     expand=True,
+# )
+# tk.Button(app,
+#         text="Quit",
+#         command=app.destroy
+#         ).pack(
+#             fill=tk.BOTH,
+#             expand=True,)
