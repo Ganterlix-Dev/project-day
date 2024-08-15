aeb6acec30709035c319375712e25d0b825e721e
Gabriel moreno <Ganterlix-Dev@users.noreply.github.com>
2024-08-15 15:40:51 +0200

    avance de la ventana

--- windows.py
+++ windows.py
@@ -7,14 +7,29 @@ from tkinter import messagebox
 import mysql.connector
 import tkinter as tk 
 
-app = tk.Tk()
-tk.Wm.wm_title(app, 'Administracion')
-frm = ttk.Frame(app, padding=10)
+class Ventana:
+    def __init__(self,root):
+        self.root=root
+        self.root.title('Manejo de sistema')
+        self.root.geometry('800x600')
+        
+        lbltitle=Label(self.root,bd=20,relief=RIDGE,text='manage students',fg='red',bg='white',font=('times new roman',30,'bold'))
+        lbltitle.pack(side=TOP,fill=X)
+        
+
+root=Tk()
+ob=Ventana(root)
+root.mainloop()
 
 
-app.geometry('1000x800') #dimenciones
-app.configure(background='black') #fondo
-app.mainloop()
+# app = tk.Tk()
+# tk.Wm.wm_title(app, 'Administracion')
+# frm = ttk.Frame(app, padding=10)
+
+
+# app.geometry('1000x800') #dimenciones
+# app.configure(background='black') #fondo
+# app.mainloop()
 
 # def saludar():
 #     print('hola')
